d50af857764dfc178592a9c5d7a723c60b61646c
Bellenoue <bruno.bellenoue@gmail.com>
2020-04-28 14:48:47 +0200

    Fix pipe-solution validation pipe

--- tp/apps/pipe-solution/src/users/dto/create-user.dto.ts
+++ tp/apps/pipe-solution/src/users/dto/create-user.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly firstname: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly lastname: string;
+
+  @IsEmail()
+  @IsString()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
+
   readonly age: number;
 }
